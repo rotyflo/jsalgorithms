af607ca684418d7ca5b2f0ebdd33daf3bb035be5
RT Flores <rotyflo@gmail.com>
2018-12-25 00:13:33 -0800

    Find largest numbers in multiple arrays

--- basic-algorithms.js
+++ basic-algorithms.js
@@ -38,3 +38,26 @@ function findLongestWordLength(str) {
 // console.log(
 //   findLongestWordLength("The quick brown fox jumped over the lazy dog")
 // )
+
+function largestOfFour(arr) {
+  let largestNumbers = []
+
+  arr.forEach(subArr => {
+    let largest = subArr[0]
+
+    subArr.forEach(num => {
+      if (num > largest) largest = num
+    })
+
+    largestNumbers.push(largest)
+  })
+
+  return largestNumbers
+}
+
+// console.log(largestOfFour([
+//   [4, 5, 1, 3],
+//   [13, 27, 18, 26],
+//   [32, 35, 37, 39],
+//   [1000, 1001, 857, 1]
+// ]))
