1a9213b8a8372a3d75e3036b0d7583cc9a98d137
RT Flores <rotyflo@gmail.com>
2018-12-27 13:51:28 -0800

    Enable user to stay in program for as long as they want

--- main.js
+++ main.js
@@ -4,25 +4,43 @@ const getParams = require('get-parameter-names')
 
 let algoNames = Object.keys(algorithms)
 
-console.log('\nLIST OF FUNCTIONS')
+while (true) {
+  console.clear()
 
-for (let i in algoNames) {
-  console.log(`  ${i}. ${algoNames[i]}`)
+  console.log("LIST OF FUNCTIONS\n(enter 'q' to quit)\n")
+
+  for (let i in algoNames) {
+    console.log(`\t${i}. ${algoNames[i]}`)
+  }
+
+  let index = rl.question('\nPick a number: ')
+
+  if (index === 'q') {
+    console.clear()
+    break
+  }
+
+  let funcName = algoNames[index]
+  let func = algorithms[funcName]
+
+  console.clear()
+  
+  while (true) {
+    console.log('\n' + funcName)
+
+    let params = getParams(func)
+    let args = []
+
+    for (let param of params) {
+      let arg = rl.question(param + ': ')
+
+      args.push(arg)
+
+      if (arg === 'q') break
+    }
+
+    if (args[0] === 'q') break
+
+    console.log('result: ' + func(args[0], args[1], args[2], args[3]))
+  }
 }
-
-let index = rl.question('\nPick a number: ')
-let funcName = algoNames[index]
-let func = algorithms[funcName]
-
-console.log('\n' + funcName)
-
-let params = getParams(func)
-let args = []
-
-for (let param of params) {
-  let arg = rl.question(param + ': ')
-
-  args.push(arg)
-}
-
-console.log('result: ' + func(args[0], args[1], args[2], args[3]))
