447aba9e8873027e548a6650771c13c89d4a8d40
RT Flores <rotyflo@gmail.com>
2018-12-26 21:10:23 -0800

    Condense modules into one module

--- src/algorithms.js
+++ src/algorithms.js
@@ -1,4 +1,9 @@
 module.exports = {
+
+
+  // BASIC ALGORITHMS
+
+
   convertToF: (celsius) => {
     let fahrenheit = celsius * 9 / 5 + 32
 
@@ -35,8 +40,7 @@ module.exports = {
 
 
   largestOfFour: (arr) => {
-    // ENSURES COMPATIBILY WITH CLI
-    arr = JSON.parse(arr)
+    CLIifiy(arr)
 
     let largestNumbers = []
 
@@ -79,7 +83,7 @@ module.exports = {
     return num >= str.length ? str : shortString
   },
 
-
+  
   findElement: (arr, func) => {
     arr = JSON.parse(arr)
     func = JSON.parse(func)
@@ -87,7 +91,7 @@ module.exports = {
     for (let num of arr) if (func(num)) return num
   },
 
-
+  
   booWho: (bool) => {
     return typeof bool === 'boolean' ? true : false
   },
@@ -157,5 +161,86 @@ module.exports = {
     }
 
     return subArrays
+  },
+
+
+  // INTERMEDIATE ALGORITHMS
+
+  
+  
+
+
+  // PROJECTS
+
+
+  palindrome: (str) => {
+    let alphanumeric = /[a-z0-9]/
+    let characters = str.toLowerCase().split('')
+    let alphaNumStr = ''
+    let reverseStr = ''
+
+    characters.forEach(char => {
+      if (char.match(alphanumeric)) {
+        alphaNumStr += char
+        reverseStr = char + reverseStr
+      }
+    })
+
+    let isPalindrome = alphaNumStr === reverseStr
+
+    return isPalindrome
+  },
+
+
+  convertToRoman: (num) => {
+    // ENSURES COMPATIBILITY WITH CLI
+    num = Number(num)
+
+    const romanNums = {
+      1000: 'M',
+      900: 'CM',
+      500: 'D',
+      400: 'CD',
+      100: 'C',
+      90: 'XC',
+      50: 'L',
+      40: 'XL',
+      10: 'X',
+      9: 'IX',
+      5: 'V',
+      4: 'IV',
+      1: 'I'
+    }
+    let convertedNum = ''
+    let decimalVals = Object.keys(romanNums).reverse()
+
+    decimalVals.forEach(decimalVal => {
+      while (num >= decimalVal) {
+        let romanNum = romanNums[decimalVal]
+
+        convertedNum += romanNum
+        num -= decimalVal
+      }
+    })
+
+    return convertedNum
+  },
+
+
+  rot13: (str) => {
+    let decodedStr = ''
+
+    for (let i = 0; i < str.length; i++) {
+      let charCode = str.charCodeAt(i)
+      let decodedCode = charCode
+
+      if (charCode >= 65 && charCode <= 90) {
+        decodedCode = charCode < 78 ? charCode + 13 : charCode - 13
+      }
+      
+      decodedStr += String.fromCharCode(decodedCode)
+    }
+
+    return decodedStr
   }
 }
