0b6a2034ecb35eefa23b06f48de57211404f28aa
RT Flores <rotyflo@gmail.com>
2019-01-01 23:38:03 -0600

    Add semicolons, hoist function

--- main.js
+++ main.js
@@ -1,69 +1,69 @@
-const algorithms = require('./src/algorithms.js')
-const rl = require('readline-sync')
-const getParams = require('get-parameter-names')
+const algorithms = require('./src/algorithms.js');
+const rl = require('readline-sync');
+const getParams = require('get-parameter-names');
 
-let algoNames = Object.keys(algorithms)
-
-let formatFuncName = (func) => {
-  let titledFunc = ''
-
-  for (let i in func) {
-    let char = func[i]
-
-    if (char === char.toUpperCase()) {
-      titledFunc += ' ' + char
-    }
-    else {
-      titledFunc += char.toUpperCase()
-    }
-  }
-
-  return titledFunc
-}
+let algoNames = Object.keys(algorithms);
 
 while (true) {
-  console.clear()
+  console.clear();
 
-  console.log("LIST OF FUNCTIONS\n(enter 'q' to quit)\n")
+  console.log("LIST OF FUNCTIONS\n(enter 'q' to quit)\n");
 
   for (let i in algoNames) {
-    let menuItem = i < 10 ? '\t ' : '\t'
+    let menuItem = i < 10 ? '\t ' : '\t';
 
-    menuItem += `${i}  ${formatFuncName(algoNames[i])}`
+    menuItem += `${i}  ${formatFuncName(algoNames[i])}`;
     
-    console.log(menuItem)
+    console.log(menuItem);
   }
 
-  let index = rl.question('\nPick a number: ')
+  let index = rl.question('\nPick a number: ');
 
   if (index === 'q') {
-    console.clear()
-    break
+    console.clear();
+    break;
   }
 
-  let funcName = algoNames[index]
-  let func = algorithms[funcName]
+  let funcName = algoNames[index];
+  let func = algorithms[funcName];
 
-  console.clear()
+  console.clear();
 
-  console.log(formatFuncName(funcName))
-  console.log("(enter 'q' to return to menu)\n")
+  console.log(formatFuncName(funcName));
+  console.log("(enter 'q' to return to menu)\n");
 
   while (true) {
-    let params = getParams(func)
-    let args = []
+    let params = getParams(func);
+    let args = [];
 
     for (let param of params) {
-      let arg = rl.question(param + ': ')
+      let arg = rl.question(param + ': ');
 
-      args.push(arg)
+      args.push(arg);
 
-      if (arg === 'q') break
+      if (arg === 'q') break;
     }
 
-    if (args[0] === 'q') break
+    if (args[0] === 'q') break;
 
-    console.log('result: ' + func(args[0], args[1], args[2], args[3]))
-    console.log()
+    console.log('result: ' + func(args[0], args[1], args[2], args[3]));
+    console.log();
   }
+}
+
+function formatFuncName(func) {
+  let titledFunc = '';
+
+  for (let i in func) {
+    let char = func[i];
+
+    if (char === char.toUpperCase()) {
+      titledFunc += ' ' + char;
+    }
+    else {
+      titledFunc += char.toUpperCase();
+    }
+  }
+
+  return titledFunc;
 }
